6f9a5537ea48b2b7335c80268d7bcbc1d1d227d2
Aaron Thomas <aaronthomas.handyman@gmail.com>
2016-04-30 14:22:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+RPSUnlimited is a game designed in Python where you can create your own version of RockPaperScissors!
+
+Simply edit moves.txt with all the possible moves and RPSUnlimited will adapt to your changes!
+
+Example moves.txt
+
+Rock,Scissors,Lizard
+Paper,Rock,Spock
+Scissors,Paper,Lizard
+Lizard,Paper,Spock
+Spock,Scissors,Rock
+
+Keep in mind that RPSUnlimited will use the first character in each line to determine the name of the game.
---
+++ RPSUnlimited.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from random import randrange
+
+FILENAME = "moves.txt"
+
+class Result(Enum):
+	lose = -1
+	tie = 0
+	win = 1
+
+def loadMoves(filename):
+	with open(filename, "r") as fileObj:
+		moves = [line.strip().split(',') for line in fileObj]
+	return moves
+
+def createGameName(moves):
+	gameName = ""
+	for move in moves:
+		gameName += move[0]
+	return gameName
+
+def printChoices(moves):
+	for choiceIndex in range(len(moves)):
+		print("({}) {}".format((choiceIndex + 1), moves[choiceIndex][0]))
+
+def doesXBeatY(moves, player1, player2):
+	if(player1 == player2):
+		return Result.tie
+	if(player2 in moves[1:]):
+		return Result.win
+	else:
+		return Result.lose
+
+def main():
+	score = 0
+	moves = loadMoves(FILENAME)
+	print("Welcome to {}!".format(createGameName(moves)))
+	while True:
+		print("")
+		printChoices(moves)
+		while True:
+			try:
+				choice = int(input("Please make a choice: "))
+			except ValueError:
+				print("Choice must be an integer.")
+				continue
+			else:
+				if(choice < 1 or choice > len(moves)):
+					print("Choice must be in between 1 - {}.".format(len(moves)))
+					continue
+				else:
+					break
+		player1 = moves[choice - 1][0]
+		player2 = moves[randrange(0, len(moves) - 1)][0]
+		print("\n{} vs. {}...".format(player1, player2))
+		result = doesXBeatY(moves[choice - 1], player1, player2)
+		if(result == Result.win):
+			score += Result.win.value
+			print("{} wins!".format(player1))
+		elif(result == Result.lose):
+			score += Result.lose.value
+			print("{} wins!".format(player2))
+		else:
+			score += Result.tie.value
+			print("Tie.")
+		print("\nYour score is {}!".format(score))
+		playAgain = input("Do you want to play again? (Y/N) ")
+		if(playAgain == "N"):
+			break
+
+main()
